972c1cbd1e85cbc4a3e1072c06e6ea9443ac9a62
Itsusinn <itsusinn@meowcat.org>
2020-08-31 14:37:14 +0800

    update

--- anime-search-plugin/src/main/kotlin/org/meowcat/mirai/consoleplugin/AnimeSearchPlugin.kt
+++ anime-search-plugin/src/main/kotlin/org/meowcat/mirai/consoleplugin/AnimeSearchPlugin.kt
@@ -1,88 +1,123 @@
 package org.meowcat.mirai.consoleplugin
 
 import kotlinx.coroutines.delay
+import net.mamoe.mirai.console.command.CommandManager.INSTANCE.register
+import net.mamoe.mirai.console.command.CommandManager.INSTANCE.unregister
+import net.mamoe.mirai.console.command.CommandPermission
+import net.mamoe.mirai.console.command.CommandSender
+import net.mamoe.mirai.console.command.CompositeCommand
+import net.mamoe.mirai.console.command.MemberCommandSender
+import net.mamoe.mirai.console.data.*
 import net.mamoe.mirai.console.plugin.jvm.KotlinPlugin
+import net.mamoe.mirai.console.util.BotManager
+import net.mamoe.mirai.console.util.BotManager.INSTANCE.addManager
+import net.mamoe.mirai.console.util.ConsoleExperimentalAPI
+import net.mamoe.mirai.event.subscribeFriendMessages
 import net.mamoe.mirai.event.subscribeGroupMessages
 import net.mamoe.mirai.message.data.Image
 import net.mamoe.mirai.message.data.PlainText
-import net.mamoe.mirai.message.data.content
 import net.mamoe.mirai.message.data.queryUrl
+import org.meowcat.mirai.consoleplugin.AnimeSearchPlugin.animeSearchClient
 
 
 object AnimeSearchPlugin : KotlinPlugin() {
+    val animeSearchClient = TraceMoeService.Factory.create()
 
-    private val nbnhhshClient = NbnhhshService.Factory.create()
-    private val animeSearchClient = TraceMoeService.Factory.create()
+
     override fun onEnable() {
 
-        //能不能好好说话
-        subscribeGroupMessages {
-            always {
-                var str = this.message.content
-                if (str.startsWith("/nbnhhsh ")
-                        || str.startsWith("/能不能好好说话 ")
-                        || str.startsWith("好好说话 ")) {
-                    if (str.startsWith("/nbnhhsh ") || str.startsWith("/nbnhhsh ")) {
-                        str = str.substring(9)
-                    } else {
-                        str = str.substring(5)
-                    }
-                    val guess = nbnhhshClient.guess(mapOf("text" to str))
-                    val resultAnswers: StringBuilder = java.lang.StringBuilder()
-                    val results = guess.execute().body()?.get(0)?.trans
-                    results?.forEach {
-                        resultAnswers.append("\n" + it)
-                    }
-                    reply("${str}的意思是$resultAnswers")
-                }
-            }
+        MyPluginData.reload()
+        MyCompositeCommand.register()
 
-            //echo
-            startsWith("echo "){
-                val text = this.message.content.substring(5)
-                delay(200)
-                reply(text)
-            }
+    }
 
+    override fun onDisable() {
 
-            //动画搜图
-            startsWith("as"){
-                val image = this.message[Image]
-                if (image != null) {
-                    val url = image.queryUrl()
-                    delay(200)
-                    reply("查询中")
-                    val aniResults = animeSearchClient.search(url).execute().body()?.docs
-                    reply("查询完毕")
-                    val aniResult = aniResults!![0]
-                    val replyText = StringBuilder()
-                    var time1 = aniResult.at / 60
-                    var time2 = time1.toInt().toFloat()
-                    PlainText
-                    time1 -= time2
-                    time1 *= 60
-                    replyText.append("""
+        MyCompositeCommand.unregister()
+
+    }
+
+}
+
+object MySetting : AutoSavePluginConfig() {
+
+//    val name by value("test")
+//    var count by value(0)
+
+    val isEnabled by value(mutableListOf(1L,2L))
+
+}
+object MyPluginData : AutoSavePluginData() {
+
+//    var list: MutableList<String> by value(mutableListOf("a", "b")) // mutableListOf("a", "b") 是初始值, 可以省略
+//    var long: Long by value(0L) // 允许 var
+//    var int by value(0) // 可以使用类型推断, 但更推荐使用 `var long: Long by value(0)` 这种定义方式.
+
+}
+
+// 复合指令
+@OptIn(ConsoleExperimentalAPI::class)
+object MyCompositeCommand : CompositeCommand(
+    AnimeSearchPlugin, "as",
+    description = "动漫搜索", permission = MyCustomPermission,
+    // prefixOptional = true // 还有更多参数可填, 此处忽略
+) {
+
+    // /as search <一张图片>
+    // 支持 Image 类型, 需在聊天中执行此指令.
+    @SubCommand
+    suspend fun CommandSender.search(image: Image){
+        val url = image.queryUrl()
+        delay(200)
+        sendMessage("查询中")
+        val aniResults = animeSearchClient.search(url).execute().body()?.docs
+        sendMessage("查询完毕")
+        val aniResult = aniResults!![0]
+        val replyText = StringBuilder()
+        var time1 = aniResult.at / 60
+        var time2 = time1.toInt().toFloat()
+        PlainText
+        time1 -= time2
+        time1 *= 60
+        replyText.append("""
                         番剧名称：${aniResult.anime}
                         上映时间：${aniResult.season}
                         出现集数：${aniResult.episode}
                         出现时间：${time2}分${time1}秒
                         相似度：${aniResult.similarity}
                     """.trimIndent())
-                    reply(replyText.toString())
-                    delay(1500)
-                }else{
-                    reply("请在as后添加图片");
-                    return@startsWith
-                }
-
-            }
-        }
-
-
-
+        sendMessage(replyText.toString())
+        delay(1500)
     }
 
-    override fun onDisable() {
+    //开启
+    @Permission(CommandPermission.Any::class)
+    @SubCommand
+    suspend fun CommandSender.enable(){
+        if (this is MemberCommandSender){9098
+            MySetting.isEnabled.add(this.group.id)
+            sendMessage("动画搜索 enabled")
+        }
+    }
+    //关闭
+    @Permission(CommandPermission.Any::class)
+    @SubCommand
+    suspend fun CommandSender.disable(){
+        if (this is MemberCommandSender){
+            MySetting.isEnabled.remove(this.group.id)
+            sendMessage("动画搜索 disabled")
+        }
+    }
 
+}
+
+// 定义自定义指令权限判断
+object MyCustomPermission : CommandPermission {
+    override fun CommandSender.hasPermission(): Boolean {
+
+        return if(this is MemberCommandSender){
+//            MySetting.isEnabled.contains(this.group.id)
+            true
+        }else true
     }
 }
--- anime-search-plugin/src/test/kotlin/RunConsole.kt
+++ anime-search-plugin/src/test/kotlin/RunConsole.kt
@@ -1,9 +1,6 @@
-
-import net.mamoe.mirai.console.plugin.PluginManager.INSTANCE.enable
 import net.mamoe.mirai.console.pure.MiraiConsolePureLoader
-import org.meowcat.mirai.consoleplugin.AnimeSearchPlugin
 
 fun main() {
+
     MiraiConsolePureLoader.main(arrayOf())
-    AnimeSearchPlugin.enable() // 主动启用插件
 }
